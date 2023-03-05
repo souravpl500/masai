61a5e18005780ba7192c688b790deddbae4319e6
souravpl500 <111083486+souravpl500@users.noreply.github.com>
2023-03-05 23:53:43 +0530

    Delete scores.model.js

--- models/scores.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const scoreSchema = mongoose.Schema({
-  name: { type: String, require: true },
-  diff: { type: String, require: true },
-  score: { type: Number, require: true },
-});
-const scoreModel = mongoose.model("score", scoreSchema);
-module.exports = { scoreModel };
