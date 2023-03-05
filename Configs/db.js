48f8bacbc5fd1d90dda9bdc79a8c40274c079246
souravpl500 <111083486+souravpl500@users.noreply.github.com>
2023-03-06 00:03:44 +0530

    Update db.js

--- Configs/db.js
+++ Configs/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
-
-const connection = mongoose.connect("mongodb+srv://souravpl500:Souravpl500@cluster0.hlt1sxm.mongodb.net/mock?retryWrites=true&w=majority");
+mongoose.set("strictQuery", false);
+const connection = mongoose.connect(
+  "mongodb+srv://souravpl500:Souravpl500@cluster0.hlt1sxm.mongodb.net/mock?retryWrites=true&w=majority"
+);
 
 module.exports = { connection };
