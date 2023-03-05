36eeb596281085bb75cede0ab9ff482e559a78c6
souravpl500 <111083486+souravpl500@users.noreply.github.com>
2023-03-05 23:53:52 +0530

    Delete word.model.js

--- models/word.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const wordSchema = mongoose.Schema({
-  word: { type: String, require: true },
-});
-const wordModel = mongoose.model("word", wordSchema);
-module.exports = { wordModel };
