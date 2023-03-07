cdbbcc7f2b240bbd9dab623b43d56fa7b7378a66
souravpl500 <111083486+souravpl500@users.noreply.github.com>
2023-03-07 16:17:43 +0530

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  name: { type: String, require: true },
+  name: String,
   email: { type: String, require: true },
   password: { type: String, require: true }
 });
