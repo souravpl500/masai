983c8a2d4ca9bf4e1e5a19a20106dbdf182f6cd4
souravpl500 <111083486+souravpl500@users.noreply.github.com>
2023-03-08 00:09:41 +0530

    Delete db.js

--- Configs/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-const connection = mongoose.connect(
-  "mongodb+srv://souravpl500:Souravpl500@cluster0.hlt1sxm.mongodb.net/masai?retryWrites=true&w=majority"
-);
-
-module.exports = { connection };
