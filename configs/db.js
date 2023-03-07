25538923a595839e3ea586a5c9fc12603ea77b85
souravpl500 <111083486+souravpl500@users.noreply.github.com>
2023-03-07 15:20:50 +0530

    Update db.js

--- configs/db.js
+++ configs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const connection = mongoose.connect(
-  "mongodb+srv://souravpl500:Souravpl500@cluster0.hlt1sxm.mongodb.net/mock?retryWrites=true&w=majority"
+  "mongodb+srv://souravpl500:Souravpl500@cluster0.hlt1sxm.mongodb.net/masai?retryWrites=true&w=majority"
 );
 
 module.exports = { connection };
